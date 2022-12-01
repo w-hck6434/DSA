371db8f455c899041fb2d80bfe48d89a1ee7c2f1
w-hck6434 <113173460+w-hck6434@users.noreply.github.com>
2022-12-01 10:44:51 +0530

    Delete practical01.py

--- practical01.py
+++
@@ -1,3 +0,0 @@
-Python 3.10.3 (tags/v3.10.3:a342a49, Mar 16 2022, 13:07:40) [MSC v.1929 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
-print ("Ishan")
